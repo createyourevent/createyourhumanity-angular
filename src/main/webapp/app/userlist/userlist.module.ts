4fe4febe6ac86c7aa1b2e1e0ea6d798b7e487f41
Manuel Matthias Miklo <manuelmiklo@hotmail.com>
2024-02-05 21:48:14 +0100

    div updates

--- src/main/webapp/app/userlist/userlist.module.ts
+++ src/main/webapp/app/userlist/userlist.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserlistComponent } from './userlist.component';
-import { ButtonModule } from 'primeng/button';
+import { ButtonModule } from 'primeng';
 import { DataViewModule } from 'primeng/dataview';
 import { DropdownModule } from 'primeng/dropdown';
 import { InputTextModule } from 'primeng/inputtext';
